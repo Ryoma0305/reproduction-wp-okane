7ba3433171db04b67c5d5fb73348aa7f3136f78e
Ryoma0305 <ryomaaa0602@gmail.com>
2022-06-10 10:51:48 +0900

    delete files

--- src/wp/themes/okaneships/modules/side-search.php
+++
@@ -1,4 +0,0 @@
-			<form action="<?php echo get_permalink(get_page_by_path('list')->ID); ?>" method="get" class="m-side-search">
-				<input type="text" name="s" placeholder="キーワードで検索する" class="m-side-search__input">
-				<button type="submit" class="m-side-search__btn"></button>
-			</form>
